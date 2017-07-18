52b8b3568fbb34a4dfb2fde7a604f8d73b769373
Alexandre DUVAL <alexandre.duval@clever-cloud.com>
2017-07-18 09:55:54 +0200

    change some definition

--- server.js
+++ server.js
@@ -1,8 +1,5 @@
 const http = require("http");
 
-const hostname = "0.0.0.0";
-const port = process.env.PORT | 3000;
-
 const server = http.createServer((req, res) => {
     console.log("-------------------------");
     console.log(req);
@@ -15,6 +12,6 @@ const server = http.createServer((req, res) => {
     res.end("Received\n");
 });
 
-server.listen(port, hostname, () => {
+server.listen(process.env.PORT, "0.0.0.0", () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
